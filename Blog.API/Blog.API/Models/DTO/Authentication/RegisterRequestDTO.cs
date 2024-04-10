e1d70fe09024c6276df4681b4a37c485addb3256
mashalaneevanspheletso@gmail.com <mashalaneevanspheletso@gmail.com>
2024-04-10 23:35:09 +0200

    added Authentication folder for domain-models

---
+++ Blog.API/Blog.API/Models/DTO/Authentication/RegisterRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.API.Models.DTO.Authentication
+{
+    public class RegisterRequestDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
